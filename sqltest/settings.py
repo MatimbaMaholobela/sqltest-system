6212dc9852cbc35bbbdb1276d086c60bf132e4c4
Matimba Maholobela <mhlmat029@myuct.ac.za>
2023-10-26 06:40:55 +0200

    update settings

--- sqltest/settings.py
+++ sqltest/settings.py
@@ -137,15 +137,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+# Define the STATIC_ROOT where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
-#MEDIA_URL = '/media/'
 
+# Define the MEDIA_URL and MEDIA_ROOT for serving and storing media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
