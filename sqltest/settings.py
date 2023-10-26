5ca72ecf6693084121f9d23381240acdd5a7562f
Matimba Maholobela <mhlmat029@myuct.ac.za>
2023-10-26 06:08:10 +0200

    update settings

--- sqltest/settings.py
+++ sqltest/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,7 +149,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Heroku settings
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'home','media')
