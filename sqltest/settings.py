b606e1e609f12e505bf3f4208d32a2127dff0107
Matimba Maholobela <89155223+MatimbaMaholobela@users.noreply.github.com>
2024-03-22 14:03:14 +0200

    Update settings.py

--- sqltest/settings.py
+++ sqltest/settings.py
@@ -30,8 +30,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'  # e.g., 'smtp.gmail.com'
 EMAIL_PORT = 587  # Port number for your email host
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sqltest23@outlook.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'PWD4LFMADU73195'#outlook pass
+EMAIL_HOST_USER = ''  # Your email address
+EMAIL_HOST_PASSWORD = ''#outlook pass
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','*']
 
